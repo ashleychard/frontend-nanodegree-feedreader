b4100c3189b57630446836571b602a627a16dacc
Ashley Chard <ashleychard@users.noreply.github.com>
2018-09-04 22:45:17 -0700

    Added new tests for the feed reader.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,46 +27,136 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* 
+         * Test that URLs are defined and the URL is not empty.
+         * Wrapping this in a describe block for better readability.
+         * Splitting the required tests into two parts so it is easier to
+         * tell where the error is. 
+         * First, check that the URL for each is defined.
+         * Next, see that there is actual content in the URL
          */
 
+         describe('Element URLs', function(){
+             allFeeds.forEach((feed, index)=>{
+              it(` Index ${index} (${feed.name}) is defined`, function(){
+                expect(feed.url).toBeDefined();
+             });
+           });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+           allFeeds.forEach((feed, index)=>{
+              it(` Index ${index} (${feed.name}) has a URL: ${feed.url}`, function(){
+                expect(feed.url).not.toBe('');
+             });
+           });
+         });
+        
+
+         
+
+        /* Tests each feedin the allFeeds object and ensures
+         * it has a name defined and that the name is not empty.
+         * 
+         * NOTE: This could easily be combined with the test above
+         * in an "elements" function or something similar. 
+         * However, I am following the instructions. 
          */
+         
+
+         describe(`Element Names`,function(){
+
+          allFeeds.forEach((feed, index) =>{
+            it(`Element ${index} is defined`,function(){
+              expect(feed.name).toBeDefined();
+            });
+          });
+
+          allFeeds.forEach((feed, index) =>{
+            it(`Element ${index} has a name of ${feed.name}`,function(){
+              expect(feed.name).not.toBe('');
+            });
+          });
+
+      });
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe(`The menu`, function(){
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+      /* TODO: Write a test that ensures the menu element is
+       * hidden by default. You'll have to analyze the HTML and
+       * the CSS to determine how we're performing the
+       * hiding/showing of the menu element.
+       */       
+      describe(`default behavior`, function(){
+        var body = document.querySelector("body");
+        it(`is to hide by default`,function(){
+          expect(body.classList.contains("menu-hidden")).toBe(true);
+        });
+       });
+
+       /* Ensures the menu changes visibility when the menu 
+        * icon is clicked. This test has two expectations:
+        * the menu displays when clicked 
+        * the menu hides when clicked again.
+        */  
+      describe(`click behavior`, function(){
+        var body = document.querySelector("body");
+        var menuButton = document.querySelector(".icon-list");
+
+        it(`is to open when clicked while closed`, function(){
+          menuButton.click();
+          expect(body.classList.contains("menu-hidden")).toBe(false);
+          menuButton.click();
+        });
+
+        it(`is to close when clicked while open`, function(){
+          menuButton.click();
+          menuButton.click();
+          expect(body.classList.contains("menu-hidden")).toBe(true);
+        });
+      });  
+
+    });
+        
+    /*Tests to see if there is at least one element loaded upon boot*/
+    describe("Initial Entries", function(){
+      /* Ensures when the loadFeed function is called 
+       * and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       */
+       beforeEach(function(done){
+        loadFeed(0,done);
+       });
+
+       it(`has at least one .entry element within the .feed container`, function(){
+        var feed = document.querySelector(".feed");
+        expect(feed.children.length > 0).toBe(true);
+       });
+
+    });
+
+    /* Checks new feed functionality */
+    describe(`New feed selection`, function(){
+        /* Ensures when a new feed is loaded by the loadFeed function,
+         * that the content actually changes.
          */
+         const feed = document.querySelector(".feed");
+         var initialContent;
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+      beforeEach(function(done){
+        loadFeed(0);
+        initialContent = feed.innerText
+        loadFeed(1,done);
+      });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      it(`loads a new feed`, function(){
+        const newFeed = feed.innerText;
+        expect(newFeed === initialContent).toBe(false);
+      });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 }());
